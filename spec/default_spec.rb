0afd13042e1ac4b83be24000037db218a0554ba7
JJ Asghar <jj@getchef.com>
2014-11-21 15:45:45 -0600

    WIP: got the converge report working

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,9 +1,22 @@
 require 'spec_helper'
 
+
 describe 'rabbitmq::default' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
-  it 'installs logrotate' do
-    expect(chef_run).to install_package('logrotate')
+  it 'creates a directory for mnesiadir' do
+    expect(chef_run).to create_directory(node['rabbitmq']['mnesiadir'])
   end
+
+   it 'creates a template with attributes' do
+    expect(chef_run).to create_template("#{node['rabbitmq']['config_root']}/rabbitmq-env.conf").with(
+      user:   'root',
+      group:  'root',
+      backup: false,
+      source: 'rabbit-env.conf.erb',
+      mode: 00644
+
+                        ).to notify("service[#{node['rabbitmq']['service_name']}]").to(:restart).immediately
+
+   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,12 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+ChefSpec::Coverage.start!
+
+RSpec.configure do |config|
+  config.platform = 'redhat'
+  config.version = '6.5'
+end
+
+
 
 at_exit { ChefSpec::Coverage.report! }
